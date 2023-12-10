ea5f411c938b470a992b1771f7ca86a04d3edcf1
Lofiat <75358872+dyeddaboi@users.noreply.github.com>
2023-12-10 13:45:11 -0600

    oopsie no Brackey

--- Plugin.cs
+++ Plugin.cs
@@ -37,8 +37,8 @@ namespace CubeSummoner
 			MyCubeL.transform.position = new Vector3(0, 0, 0);
 			MyCubeR = GameObject.CreatePrimitive(PrimitiveType.Cube);
 			MyCubeR.transform.position = new Vector3(0, 0, 0);
-   			MyCubeL.name = "MyCubeL";
-      			MyCubeR.name = "MyCubeR";
+			MyCubeL.name = "MyCubeL";
+			MyCubeR.name = "MyCubeR";
 			MyCubeR.transform.localScale = new Vector3(0.5f, 0.175f, 0.5f);
 			MyCubeL.transform.localScale = new Vector3(0.5f, 0.175f, 0.5f);
 			CubeMaterialR = new Material(Shader.Find("GorillaTag/UberShader"));
@@ -46,63 +46,64 @@ namespace CubeSummoner
 			MyCubeR.GetComponent<Renderer>().material = CubeMaterialR;
 			MyCubeL.GetComponent<Renderer>().material = CubeMaterialL;
 			Instantiate(MyCubeL);
-   			Instantiate(MyCubeR);
-      
-		void Update()
-		{
-			/* Code here runs every frame when the mod is enabled */
-			if (inRoom)
+			Instantiate(MyCubeR);
+
+			void Update()
 			{
+				/* Code here runs every frame when the mod is enabled */
+				if (inRoom)
+				{
 
-				if (ControllerInputPoller.instance.rightControllerGripFloat >= 0.5f)
-				{
-					if (posSetR == false)
+					if (ControllerInputPoller.instance.rightControllerGripFloat >= 0.5f)
 					{
-						MyCubeR.transform.position = new Vector3(GorillaLocomotion.Player.Instance.rightControllerTransform.position.x, GorillaLocomotion.Player.Instance.rightControllerTransform.position.y, GorillaLocomotion.Player.Instance.rightControllerTransform.position.z);
-						posSetR = true;
+						if (posSetR == false)
+						{
+							MyCubeR.transform.position = new Vector3(GorillaLocomotion.Player.Instance.rightControllerTransform.position.x, GorillaLocomotion.Player.Instance.rightControllerTransform.position.y, GorillaLocomotion.Player.Instance.rightControllerTransform.position.z);
+							posSetR = true;
+						}
+						GorillaLocomotion.Player.Instance.rightControllerTransform.position = new Vector3(MyCubeR.transform.position.x, MyCubeR.transform.position.y, MyCubeR.transform.position.z);
 					}
-					GorillaLocomotion.Player.Instance.rightControllerTransform.position = new Vector3(MyCubeR.transform.position.x, MyCubeR.transform.position.y, MyCubeR.transform.position.z);
-				}
-				else
-				{
-					posSetR = false;
-					MyCubeR.transform.position = new Vector3(0, 0, 0);
-				}
-				if (ControllerInputPoller.instance.leftControllerGripFloat >= 0.5f)
-				{
-					if (posSetL == false)
+					else
 					{
-						MyCubeL.transform.position = new Vector3(GorillaLocomotion.Player.Instance.leftControllerTransform.position.x, GorillaLocomotion.Player.Instance.leftControllerTransform.position.y, GorillaLocomotion.Player.Instance.leftControllerTransform.position.z);
-						posSetL = true;
+						posSetR = false;
+						MyCubeR.transform.position = new Vector3(0, 0, 0);
 					}
-					GorillaLocomotion.Player.Instance.leftControllerTransform.position = new Vector3(MyCubeL.transform.position.x, MyCubeL.transform.position.y, MyCubeL.transform.position.z);
-				}
-				else
-				{
-					posSetL = false;
-					MyCubeL.transform.position = new Vector3(0, 0, 0);
-				}
+					if (ControllerInputPoller.instance.leftControllerGripFloat >= 0.5f)
+					{
+						if (posSetL == false)
+						{
+							MyCubeL.transform.position = new Vector3(GorillaLocomotion.Player.Instance.leftControllerTransform.position.x, GorillaLocomotion.Player.Instance.leftControllerTransform.position.y, GorillaLocomotion.Player.Instance.leftControllerTransform.position.z);
+							posSetL = true;
+						}
+						GorillaLocomotion.Player.Instance.leftControllerTransform.position = new Vector3(MyCubeL.transform.position.x, MyCubeL.transform.position.y, MyCubeL.transform.position.z);
+					}
+					else
+					{
+						posSetL = false;
+						MyCubeL.transform.position = new Vector3(0, 0, 0);
+					}
 
+				}
 			}
-		}
-		/* This attribute tells Utilla to call this method when a modded room is joined */
-		[ModdedGamemodeJoin]
-		public void OnJoin(string gamemode)
-		{
-			/* Activate your mod here */
-			/* This code will run regardless of if the mod is enabled*/
-			/* Make sure all code is going to be here or else your mod will be considered a cheat.*/
-			inRoom = true;
-		}
+			/* This attribute tells Utilla to call this method when a modded room is joined */
+			[ModdedGamemodeJoin]
+			public void OnJoin(string gamemode)
+			{
+				/* Activate your mod here */
+				/* This code will run regardless of if the mod is enabled*/
+				/* Make sure all code is going to be here or else your mod will be considered a cheat.*/
+				inRoom = true;
+			}
 
-		/* This attribute tells Utilla to call this method when a modded room is left */
-		[ModdedGamemodeLeave]
-		public void OnLeave(string gamemode)
-		{
-			/* Deactivate your mod here */
-			/* This code will run regardless of if the mod is enabled*/
+			/* This attribute tells Utilla to call this method when a modded room is left */
+			[ModdedGamemodeLeave]
+			public void OnLeave(string gamemode)
+			{
+				/* Deactivate your mod here */
+				/* This code will run regardless of if the mod is enabled*/
 
-			inRoom = false;
+				inRoom = false;
+			}
 		}
 	}
 }
