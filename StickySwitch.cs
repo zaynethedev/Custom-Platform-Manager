546100d74b3fbd36bd46882c38d203d78cd3b346
Lofiat <75358872+dyeddaboi@users.noreply.github.com>
2024-01-05 08:43:37 -0600

    Added Sticky switch from dev branch

---
+++ StickySwitch.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomPlatformManager;
+using UnityEngine;
+using TMPro;
+using System.EnterpriseServices;
+using System.ComponentModel;
+
+namespace CustomPlatformManager.buttons
+{
+    internal class shapechange : GorillaPressableButton
+    {
+        public static shapechange Instance;
+        public static string shape = "Cube";
+        public TMP_Text shapetext = Plugin.Instance.CustomPlatformManager.transform.Find("Computer/Shape/ShapeValue")?.gameObject.GetComponent<TextMeshPro>();
+
+        public override void Start()
+        {
+            Instance = this;
+            shapetext.text = shape;
+            BoxCollider boxCollider = GetComponent<BoxCollider>();
+            boxCollider.isTrigger = true;
+            gameObject.layer = 18;
+            onPressButton = new UnityEngine.Events.UnityEvent();
+            onPressButton.AddListener(new UnityEngine.Events.UnityAction(ButtonActivation));
+        }
+
+        public override void ButtonActivation()
+        {
+            isOn = !isOn;
+            string gameObjectName = gameObject.name;
+
+            switch (gameObjectName)
+            {
+                case "Circle":
+                    shape = "Sphere";
+                    shapetext.text = shape;
+                    Destroy(Plugin.Instance.CustomPlatL);
+                    Destroy(Plugin.Instance.CustomPlatR);
+                    Plugin.Instance.CustomPlatL = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    Plugin.Instance.CustomPlatR = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    Destroy(Plugin.Instance.CustomPlatL.GetComponent<SphereCollider>());
+                    Destroy(Plugin.Instance.CustomPlatR.GetComponent<SphereCollider>());
+                    Plugin.Instance.CustomPlatR.AddComponent<BoxCollider>();
+                    Plugin.Instance.CustomPlatL.AddComponent<BoxCollider>();
+                    break;
+
+                case "Square":
+                    shape = "Cube";
+                    shapetext.text = shape;
+                    Destroy(Plugin.Instance.CustomPlatL);
+                    Destroy(Plugin.Instance.CustomPlatR);
+                    Plugin.Instance.CustomPlatL = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    Plugin.Instance.CustomPlatR = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    Destroy(Plugin.Instance.CustomPlatL.GetComponent<BoxCollider>());
+                    Destroy(Plugin.Instance.CustomPlatR.GetComponent<BoxCollider>());
+                    Plugin.Instance.CustomPlatR.AddComponent<BoxCollider>();
+                    Plugin.Instance.CustomPlatL.AddComponent<BoxCollider>();
+                    break;
+
+                case "Triangle":
+                    shape = "Soon!";
+                    shapetext.text = shape;
+                    Destroy(Plugin.Instance.CustomPlatL);
+                    Destroy(Plugin.Instance.CustomPlatR);
+                    Plugin.Instance.CustomPlatL = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    Plugin.Instance.CustomPlatR = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    break;
+            }
+        }
+    }
+}
